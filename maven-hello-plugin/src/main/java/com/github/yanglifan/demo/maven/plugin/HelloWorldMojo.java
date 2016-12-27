4c37d75852cf957d20b4ff0395c906d7a08b5554
杨理帆 <yanglifan@gmail.com>
2016-12-27 21:19:26 +0800

    Provide Hello World Maven plugin Mojo

---
+++ maven-hello-plugin/src/main/java/com/github/yanglifan/demo/maven/plugin/HelloWorldMojo.java
@@ -0,0 +1,14 @@
+package com.github.yanglifan.demo.maven.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "hello", defaultPhase = LifecyclePhase.COMPILE)
+public class HelloWorldMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Hello, World!!!");
+    }
+}
